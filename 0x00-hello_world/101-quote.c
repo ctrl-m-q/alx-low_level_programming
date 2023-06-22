d0c5060ccf7abda67e10f15934f0ae6b8fc5430a
Jemimah Okhifo Quadri <qjmima@gmail.com>
2023-06-22 04:50:15 -0700

    extra tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - c program that prints exactly a line followed by a new line
+ * to the standard error
+ * Return: 1
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
