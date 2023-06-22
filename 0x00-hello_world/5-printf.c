27e9afed6c837db3179491cfc6e8b0fa919de986
Jemimah Okhifo Quadri <qjmima@gmail.com>
2023-06-22 03:31:20 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - C program that prints exactly a line followed by a line(use printf)
+ * Return:Always returns (0)
+ */
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
