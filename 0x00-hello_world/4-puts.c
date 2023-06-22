2f3c44c95212af4ef0beb88673eb6681feb0aa10
Jemimah Okhifo Quadri <qjmima@gmail.com>
2023-06-22 03:06:55 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - C program that prints line folowed by a new line
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	puts ("\" Programming is like building a multilingual puzzle");
+	return (0);
+}
